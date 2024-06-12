15b29b6d96a03c843c2b32f53e4f3dcc47feaa50
sahilghadi47 <ghadigaonkarsahil0407@gmail.com>
2024-06-12 14:53:58 +0530

    Databse connected

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(
+            `Mongo DB conntected || DB host : ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.log("MONGOOSE CONNECTION ERROR", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
