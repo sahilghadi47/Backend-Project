31a0686cfc7496f7f0b3258cd94292019d27eeba
sahilghadi47 <ghadigaonkarsahil0407@gmail.com>
2024-06-17 12:54:58 +0530

    fix user schema

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -63,7 +63,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 };
 
 userSchema.methods.generateAccessToken = function () {
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
             email: this.email,
@@ -78,7 +78,7 @@ userSchema.methods.generateAccessToken = function () {
 };
 
 userSchema.methods.generateRefreshToken = function () {
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
         },
